bda7d2d6379f3a4da72ae73421dd1d754586711c
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-09-28 16:19:45 +0600

    создал компонент для добавления предметов в заказ. В нем есть импорт React`а и типизации, интерфейс в нем типизация из импорта и функция для добавления, разметка для них

---
+++ src/components/ItemAddButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {MenuItem} from "../types";
+
+interface ItemAddButtonProps {
+    item: MenuItem;
+    onAdd: (item: MenuItem) => void;
+}
+
+const ItemAddButton: React.FC<ItemAddButtonProps> = ({item, onAdd}) => {
+    return (
+        <div className="btns-div">
+            <button
+                type="button"
+                onClick={() => onAdd(item)}
+            ><img className="image-ing"
+                  src={item.image}
+                  alt={item.name}/>
+                <div className="name-price">
+                    <span className="item-name">{item.name}</span>
+                    <span className="item-price">Price: {item.price} KGS</span>
+                </div>
+            </button>
+        </div>
+    );
+};
+
+export default ItemAddButton;
